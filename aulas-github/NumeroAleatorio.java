50ae0445832246de553ecc3371bdea631ab44a1e
fanezeleo <leofaneze14@gmail.com>
2026-05-31 02:30:31 -0300

    git-revert

--- aulas-github/NumeroAleatorio.java
+++ aulas-github/NumeroAleatorio.java
@@ -1,6 +1,6 @@
 public class NumeroAleatorio {
     public static void main(String[] args) {
-        int numeroAleatorio = (int) (Math.random() * 100000) + 1;
+        int numeroAleatorio = (int) (Math.random() * 100) + 1;
         System.out.println("Número aleatório gerado: " + numeroAleatorio);
     }
 }
