f1db1116d6726ff1d1df8fe703d6ac9db4f84bb9
fanezeleo <leofaneze14@gmail.com>
2026-06-01 03:34:02 -0300

    Aula 2: formatando textos

--- aulas-java/src/Main.java
+++ aulas-java/src/Main.java
@@ -1,10 +1,24 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Esse e i Screen Match");
-        System.out.println("Filme: Top Gun: Maverick");
+        System.out.println("Esse e o Screen Match");
+        String nomeDoFilme = "Filme: Top Gun: Maverick";
         int anoDeLancamento = 2022;
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-        boolean incluidoNoPlano = true;
 
+//        System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+
+//        System.out.println(media);
+
+        String sinopse = """
+            %s
+            Sinopse: Filme de aventura!
+            Nota do fime: %.1f
+            Ano de lançamento: %d
+            """.formatted(nomeDoFilme, notaDoFilme, anoDeLancamento);
+        System.out.println(sinopse);
     }
 }
