5635a9ff879e8f3eba12c03bf4d69acd5711a6b1
fanezeleo <leofaneze14@gmail.com>
2026-06-01 03:21:42 -0300

    Created: criação das primeiras variaveis do projeto

--- aulas-java/src/Main.java
+++ aulas-java/src/Main.java
@@ -1,5 +1,10 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Esse e i Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+
     }
 }
