321582b021e6032a3ddea83fff700c7e78135803
fanezeleo <leofaneze14@gmail.com>
2026-06-02 01:56:31 -0300

    Aula 2 java POO: criando e utilizando funções na classe

--- aulas-java/src/Filme.java
+++ aulas-java/src/Filme.java
@@ -2,7 +2,21 @@ public class Filme {
     String nomeDoFilme;
     int anoDeLancamento;
     boolean incluidoNoPlano = true;
-    double avaliacao;
+    double valorTotalAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    public void exibirFichaTecnica(){
+        System.out.println("Nome do filme: " + nomeDoFilme);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    public void adicionarAvaliacao(double nota){
+        valorTotalAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double MediaAvaliacao(){
+        return valorTotalAvaliacoes / totalDeAvaliacoes;
+    }
 }
--- aulas-java/src/Main.java
+++ aulas-java/src/Main.java
@@ -1,5 +1,12 @@
 public class Main{
     public static void main(String[] args) {
+        Filme filme = new Filme();
 
+        filme.exibirFichaTecnica();
+        filme.adicionarAvaliacao(4);
+        filme.adicionarAvaliacao(6);
+        filme.adicionarAvaliacao(8);
+
+        System.out.println(filme.MediaAvaliacao());
     }
 }
