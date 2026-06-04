44c286cf19a2ec1e7665eacbf2e66a9b81103f23
fanezeleo <leofaneze14@gmail.com>
2026-06-04 02:36:46 -0300

    Aula 6 java POO: utilizando heranças

---
+++ aulas-java/src/br/com/caldeiras/screenmatch/modelos/Serie.java
@@ -0,0 +1,40 @@
+package br.com.caldeiras.screenmatch.modelos;
+
+public class Serie extends Filme{
+    private int temporadas;
+    private boolean ativa;
+    private int episodiosPortemporada;
+    private int minutoPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodiosPortemporada() {
+        return episodiosPortemporada;
+    }
+
+    public void setEpisodiosPortemporada(int episodiosPortemporada) {
+        this.episodiosPortemporada = episodiosPortemporada;
+    }
+
+    public int getMinutoPorEpisodio() {
+        return minutoPorEpisodio;
+    }
+
+    public void setMinutoPorEpisodio(int minutoPorEpisodio) {
+        this.minutoPorEpisodio = minutoPorEpisodio;
+    }
+}
